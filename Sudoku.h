861d91b283d9cf5518cbbb1183941275a9e6f733
nox-salt <nosalty9020@gmail.com>
2024-11-25 16:01:38 +0700

    Add files via upload

---
+++ Sudoku.h
@@ -0,0 +1,13 @@
+#ifndef SUDOKU_H
+#define SUDOKU_H
+
+int DisplayMenu();
+int NewGame();
+int LoadGame();
+int Leaderboard();
+int SeleksiSudoku();
+int Exit();
+void ArrSudoku();
+int InputTidakValid();
+
+#endif
