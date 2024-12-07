9f2c0f0040a5e4d1476b278f5ac324f7290b0ed7
ouzuka <130549843+ouzuka@users.noreply.github.com>
2024-12-07 15:46:16 +0700

    Add files via upload

---
+++ Sudoku/Sudoku.h
@@ -0,0 +1,25 @@
+#include "stdbool.h"
+#ifndef SUDOKU_H
+#define SUDOKU_H
+
+int DisplayMenu();
+int NewGame();
+int Exit();
+int Leaderboard();
+int Highscore(int Score);
+int Permainan(int BONUS_SCORE, int Index, int SimpanSoal[10][9][9], int SimpanJawaban[10][9][9]);
+int Scoring(int time_start, int BASE_SCORE);
+void InputTidakValid();
+int Randomizer(int Index, int Difficulty, int SimpanSoal[10][9][9], int SimpanJawaban[10][9][9]);
+void DisplayWin();
+void DisplayLose();
+void printCentered(const char* text, int consoleWidth);
+bool CekVertikal(int SimpanSoal[10][9][9], int Index, int row, int col);
+bool CekHorizontal(int SimpanSoal[10][9][9], int Index, int row, int col);
+bool CekBlok(int SimpanSoal[10][9][9], int Index, int row, int col);
+bool CekFinal(int SimpanSoal[10][9][9], int Index, int row, int col);
+
+// void* TimerScore(void* arg);
+
+#endif
+
---
+++ Sudoku/main.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Sudoku.h"
+#include <time.h>
+#include <stdbool.h>
+#include "windows.h"
+
+int main(){
+	// menggunakan
+	enum Menu {
+		Menu_New_Game = 1,
+		Menu_Leaderboard,
+		Menu_Exit
+	};
+
+	int BASE_SCORE, Score, Index, input, brs, klm, IsiAngka, Difficulty, WaktuDetik, SimpanSoal[10][9][9], SimpanJawaban[10][9][9];
+	Difficulty = 0;
+	Score = 0;
+	bool IsiSudoku, Benar;
+	
+	while (2) { // Loop terus berjalan sampai pengguna memilih keluar
+        menu_Display:
+        input = DisplayMenu(); // Panggil fungsi untuk menampilkan menu utama
+        switch (input) {
+			case Menu_New_Game:
+				//Generate Random Angka
+				srand(time(NULL));	//Seed random
+				menu_NG:
+				Index = rand()% 9 + 1;	// Angka Random (1 - 9)
+				Difficulty = NewGame();
+				switch (Difficulty){
+					case 1:
+						BASE_SCORE = Randomizer(Index, Difficulty, SimpanSoal, SimpanJawaban);
+						input = Permainan(BASE_SCORE, Index, SimpanSoal, SimpanJawaban);
+						if(input == 3){
+							system("cls");
+							DisplayLose();
+							getchar();
+							getchar();
+							goto menu_Display;
+						}
+						if(input == 0){
+							goto menu_NG;
+						}
+						if(input == 4){
+							DisplayWin();
+							system("cls");
+							Highscore(Score);
+							goto menu_Display;
+						}
+					case 2:
+						BASE_SCORE = Randomizer(Index, Difficulty, SimpanSoal, SimpanJawaban);
+						input = Permainan(BASE_SCORE, Index, SimpanSoal, SimpanJawaban);
+						if(input == 3){
+							system("cls");
+							DisplayLose();
+							getchar();
+							getchar();
+							goto menu_Display;
+						}
+						if(input == 0){
+							goto menu_NG;
+						}
+						DisplayWin();
+						Highscore(Score);
+					case 3:
+						BASE_SCORE = Randomizer(Index, Difficulty, SimpanSoal, SimpanJawaban);
+						input = Permainan(BASE_SCORE, Index, SimpanSoal, SimpanJawaban);
+						if(input == 3){
+							input--;
+							system("cls");
+							DisplayLose();
+							getchar();
+							getchar();
+							goto menu_Display;
+						}
+						if(input == 0){
+							goto menu_NG;
+						}
+						DisplayWin();
+						Highscore(Score);
+					case 0:
+						break;
+					default:
+						InputTidakValid();
+						goto menu_NG;
+				}
+				break;
+				
+        	// Menampilkan 5 score teratas
+        	case Menu_Leaderboard:
+        		menu_L:
+            	input = Leaderboard(); // Panggil fungsi leaderboard
+            	switch(input){
+            		case 0:
+            			break;
+            		default:
+            			InputTidakValid();
+            			goto menu_L;
+				}
+            	break; // Kembali ke menu utama
+        	
+        	case Menu_Exit:
+            	Exit(); // Untuk keluar
+            	return 0;
+            
+            default:
+            	InputTidakValid();
+            	
+            	break;
+        } 
+    }
+}
